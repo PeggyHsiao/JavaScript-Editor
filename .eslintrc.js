2046f068b4f6b5a6ee318e5f581ab3fd4ae553a3
Peggy.Hsiao <Peggy.Hsiao@advantech.com.tw>
2022-05-31 13:59:36 +0800

    first commit(setting eslint)

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': ['error', 'windows'],
+    'import/extensions': ['error', 'always', { js: 'never', vue: 'never', json: 'never' }],
+  },
+};
